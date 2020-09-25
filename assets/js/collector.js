2004f6a8d7e6bd915edeccaa1cdddc345bcd8412
Matthew Lehner <matthew@mpl.io>
2020-09-25 14:59:55 -0700

    Collect page view on live view navigation
    Because LiveView uses `history.pushState` for navigation between
    multiple live views, we don't get a full page load. Instead, we listen
    for the `phx:page-loading-stop` event and check to ensure that we've had
    a "redirect" or "patch" event (not DOM events, or the initial page
    load).

--- assets/js/collector.js
+++ assets/js/collector.js
@@ -7,34 +7,44 @@
   //   return;
   // }
 
-  const hostname = window.location.host;
-  const pathname = window.location.pathname;
-  const referrer = document.referrer;
+  function trackPageView() {
+    const hostname = window.location.host;
+    const pathname = window.location.pathname;
+    const referrer = document.referrer;
 
-  const data = {
-    p: pathname,
-    h: hostname,
-    r: referrer,
-  };
+    const data = {
+      p: pathname,
+      h: hostname,
+      r: referrer,
+    };
 
-  function dataToParams(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
+    function dataToParams(data) {
+      return Object.keys(data)
+        .map(
+          (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    }
+
+    const trackerSrc = "//" + hostname + "/collect?" + dataToParams(data);
+
+    const img = document.createElement("img");
+    img.setAttribute("alt", "");
+    img.setAttribute("aria-hidden", true);
+    img.src = trackerSrc;
+
+    img.addEventListener("load", () => {
+      document.body.removeChild(img);
+    });
+
+    document.body.appendChild(img);
   }
 
-  const trackerSrc = "//" + hostname + "/collect?" + dataToParams(data);
-
-  const img = document.createElement("img");
-  img.setAttribute("alt", "");
-  img.setAttribute("aria-hidden", true);
-  img.src = trackerSrc;
-
-  img.addEventListener("load", () => {
-    document.body.removeChild(img);
+  window.addEventListener("phx:page-loading-stop", (info) => {
+    if (info.detail.kind === "redirect" || info.detail.kind === "patch") {
+      trackPageView();
+    }
   });
 
-  document.body.appendChild(img);
+  trackPageView();
 })();
