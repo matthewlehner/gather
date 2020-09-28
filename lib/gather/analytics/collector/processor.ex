4f673ee16ececbb710134b6464458f2b16850073
Dave Russell <dave@onefeather.ca>
2020-09-28 14:18:35 -0700

    Add phoenix pubsub broadcasting to analytics processor
    In order to make our dashboard update in real time (i.e. when the
    processor records the results), we will leverage phoenix's pubsub. In
    this PR the only subscriber to the broadcast will be in test.

--- lib/gather/analytics/collector/processor.ex
+++ lib/gather/analytics/collector/processor.ex
@@ -1,5 +1,27 @@
 defmodule Gather.Analytics.Collector.Processor do
   alias Gather.Analytics
+  alias Gather.Analytics.PageView
+  alias Phoenix.PubSub
 
-  def run(queue), do: Enum.each(queue, &Analytics.insert_page_view!/1)
+  def run(queue) do
+
+    for page_view <- queue do
+      page_view
+        |> Analytics.insert_page_view!()
+        |> broadcast()
+    end
+  end
+
+  def broadcast(%PageView{} = page_view) do
+    topic = "analytics"
+    event_name = :recorded
+
+    PubSub.broadcast(
+      Gather.PubSub,
+      topic,
+      {event_name, page_view}
+    )
+
+    {:ok, page_view}
+  end
 end
