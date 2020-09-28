abd2dacc6e6da19fe62d5722e329a49e2632374b
Dave Russell <dave@onefeather.ca>
2020-09-28 14:18:35 -0700

    add more pages

---
+++ lib/gather_web/live/page_live/page_live.ex
@@ -0,0 +1,29 @@
+defmodule GatherWeb.PageLive do
+  use GatherWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :circle, _) do
+    assign(socket, :page_title, "Circle!")
+  end
+
+  defp apply_action(socket, :square, _) do
+    assign(socket, :page_title, "Square!")
+  end
+
+  defp apply_action(socket, :triangle, _) do
+    assign(socket, :page_title, "Triangle!")
+  end
+
+  defp apply_action(socket, _, _) do
+    assign(socket, :page_title, "Welcome")
+  end
+end
