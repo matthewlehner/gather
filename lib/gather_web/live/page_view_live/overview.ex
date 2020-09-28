543690d3bb5efe7a75b1040c0859b9b3b9b06511
Dave Russell <dave@onefeather.ca>
2020-09-28 14:18:35 -0700

    Make analytics overview subscribe to analytics broadcast
    This PR completes the real-time update of the metrics overview
    dashboard, using some delays to make the update look a bit more obvious
    and nice

--- lib/gather_web/live/page_view_live/overview.ex
+++ lib/gather_web/live/page_view_live/overview.ex
@@ -5,6 +5,26 @@ defmodule GatherWeb.PageViewLive.Overview do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, analytics: Analytics.get_page_view_data())}
+    Analytics.subscribe()
+
+    %{page_views: views, pages: pages, referrers: referrers} = Analytics.get_page_view_data()
+
+    {:ok, assign(socket, views: views, pages: pages, referrers: referrers)}
   end
+
+  @impl true
+  def handle_info({:recorded, metric}, socket) do
+    %{pathname: pathname, referrer: referrer} = metric
+    {:noreply,
+     socket
+     |> update(:views, &(&1 + 1))
+     |> update(:pages, &(update_pages(&1, pathname)))
+     |> update(:referrers, &(update_referrers(&1, referrer)))}
+  end
+
+  defp update_pages(pages, pathname), do: Map.get_and_update(pages, pathname, &increment/1) |> elem(1)
+  defp update_referrers(referrers, referrer), do: Map.get_and_update(referrers, referrer, &increment/1) |> elem(1)
+
+  defp increment(current) when is_integer(current), do: {current, current + 1}
+  defp increment(current), do: {current, 1}
 end
