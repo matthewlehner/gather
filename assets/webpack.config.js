b42871df0269b641f64ebdb746c9580f0192be03
Matthew Lehner <matthew@mpl.io>
2021-03-07 16:39:36 -0800

    Switch to yarn and update all frontend deps

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const glob = require("glob");
-const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => {
@@ -11,10 +10,7 @@ module.exports = (env, options) => {
 
   return {
     optimization: {
-      minimizer: [
-        new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({}),
-      ],
+      minimizer: ["...", new CssMinimizerPlugin()],
     },
     entry: {
       app: glob.sync("./vendor/**/*.js").concat(["./js/app.js"]),
@@ -43,6 +39,6 @@ module.exports = (env, options) => {
     plugins: [
       new MiniCssExtractPlugin({ filename: "../css/app.css" }),
       new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] }),
-    ].concat(devMode ? [new HardSourceWebpackPlugin()] : []),
+    ],
   };
 };
